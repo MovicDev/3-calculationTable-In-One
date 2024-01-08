73a4fc1cacc09f0ea377287a4589b2bc9c3da0d8
ibrahim kolawole <86012048+MovicDev@users.noreply.github.com>
2024-01-08 05:06:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
 function calculateFactorial(){
-    if (numbe.value == "") {
-       display.innerHTML = "The space can not be empty";
+    if (factorialOp.value == "multiFac") {
 
-    } 
     var factorial = 1;
     for (var i = 1; i <= numbe.value; i++) {
        factorial *= i;
     }
     display.innerHTML = `<p class="text-dark">The Factorial of ${numbe.value} is ${factorial}</p>`;
     numbe.value = "";
-    
 }
-function calFactorial(){
-    if (numb.value == "") {
-       disp.innerHTML = "The space can not be empty";
-
-    }
+else if(factorialOp.value == "addFac"){
     var fact = 1;
-    for (var j = 1; j <= numb.value; j++) {
+    for (var j = 1; j <= numbe.value; j++) {
        fact += j;
     }
-    disp.innerHTML = `<p class="text-dark">The Factorial of ${numb.value} is ${fact - 1}</p>`;
-    numb.value = "";
+    display.innerHTML = `<p class="text-dark">The Factorial of ${numbe.value} is ${fact - 1}</p>`;
+    numbe.value = "";
     
 }
+}
+    
 function multiTable() { 
     if (firstNum.value == "" && secondNum.value == "") {
        popOut.innerHTML = "The space can not be empty";                                                                                                                                                                                                                                                                                                   
     } else{
     for (let m = 1; m <= firstNum.value; m++) {
         for (let k = 1; k <= secondNum.value; k++){  
-
-
               if (operation.value == "multi"){    
-                    popOut.innerHTML += `<p>${m} x ${k} = ${m*k}</p>`;
+                     popOut.innerHTML  += `<p>${m} x ${k} = ${m*k}</p>`;
                 } else if(operation.value  == "add"){
                     popOut.innerHTML += `<p>${m} + ${k} = ${m+k}</p>`;
                 } else if(operation.value  == "sub"){
@@ -43,11 +36,13 @@ function multiTable() {
                 } else{
                     document.getElementById("popOut").innerHTML = "Invalid";
                 } 
+          
         }
-        
+        // document.getElementById("popOut").innerHTML = "";
     }
     firstNum.value = "";
     secondNum.value ="";
     operation.value ="";
     }
+     
 }
